a12085405a9cc8d30f074647f29c267e1e0cbbe0
1ch0 <github1ch0@163.com>
2023-02-01 16:30:14 +0800

    Feat: add slice

--- rust book/chapter.04/slice/src/main.rs
+++ rust book/chapter.04/slice/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
+    let mut s = String::from("hello world");
 
+    let word = first_word(&s);
+
+    println!("The first word is: {}", word);    
+
+    let a = [1,2,3,4,5,6,7,8,9];
+    let slice = &a[1..3];
+    println!("The slice is: {:?}", slice);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
